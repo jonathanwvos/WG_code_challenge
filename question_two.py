47023d9ca849067587379b920a63713afb69b543
Jonathan W. Vos <jonathanwvos@gmail.com>
2024-06-21 19:03:29 +0200

    Completed first solution to Question 2, added a corresponding notebook, a log of the terminal commands used to generate the depth coverage distributions.

---
+++ question_two.py
@@ -0,0 +1,11 @@
+# import fastq as fq
+
+# base_reads_1 = fq.read(join(DATA_PATH, 'baseline_reads_R1.fastq'))
+
+# i = 0
+# for read in base_reads_1:
+#     print(read.getSeq())
+    
+#     if i == 3:
+#         break
+#     i +=1
